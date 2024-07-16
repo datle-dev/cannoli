d947b1b070d769156e2fd9ce80795155fd202645
datle-dev <dle.dev@outlook.com>
2024-07-16 16:25:58 -0400

    Update get to parse pk and set update to partial

--- backend/profiles/views.py
+++ backend/profiles/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
-from .models import Profile
-from .serializers import ProfileSerializer
+from rest_framework.response import Response
+from profiles.models import Profile
+from profiles.serializers import ProfileSerializer
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        pk = self.kwargs['pk']
+        profile = Profile.objects.get(user_id=pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return super().update(request, *args, **kwargs)
 
