2e5e0974d935be2f0acf046c404e16efab14496b
datle-dev <dle.dev@outlook.com>
2024-07-16 16:24:12 -0400

    Update user to be user_id with new delete scheme

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Profile(models.Model):
-    user = models.OneToOneField('authentication.User', null=True, on_delete=models.SET_NULL)
+    user_id = models.OneToOneField('authentication.User', on_delete=models.CASCADE)
     about = models.TextField(max_length=256)
     update_date = models.DateTimeField()
