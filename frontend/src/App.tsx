1eb73b5cdb7864f16b6cbb93a92df1e91ab0e5a3
datle-dev <dle.dev@outlook.com>
2024-07-16 09:34:01 -0400

    Update App.tsx with forms and auth functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import RegisterForm from "./RegisterForm";
+import LoginForm from "./LoginForm";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  async function handleRefreshToken() {
+    await fetch("http://127.0.0.1:8000/auth/token/refresh/", {
+      method: "POST",
+      body: JSON.stringify({
+        refresh: localStorage.getItem("refresh"),
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        localStorage.setItem("access", data.access);
+        localStorage.setItem("refresh", data.refresh);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  async function handleCheckUser() {
+    if (!localStorage.getItem("refresh")) return;
+
+    let res = await fetch("http://127.0.0.1:8000/auth/user/", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("access")}`,
+      },
+    });
+    let resJson = await res.json();
+
+    if (res.status === 200) {
+      console.log(`OK: ${res.status}`);
+    } else if (res.status === 401) {
+      console.log(`401 Error! ${res.status}`);
+      console.log("Refreshing token...");
+      await handleRefreshToken();
+      res = await fetch("http://127.0.0.1:8000/auth/user/", {
+        credentials: "include",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access")}`,
+        },
+      });
+      resJson = await res.json();
+      console.log("token refreshed, original req resent");
+    }
+
+    return resJson;
+  }
+
+  async function handleLogout() {
+    if (!localStorage.getItem("refresh")) return;
+
+    console.log(localStorage.getItem("refresh-token"));
+    await fetch("http://127.0.0.1:8000/auth/token/blacklist/", {
+      method: "POST",
+      body: JSON.stringify({
+        refresh: localStorage.getItem("refresh"),
+      }),
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("access")}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        localStorage.removeItem("access");
+        localStorage.removeItem("refresh");
+      })
+      .catch((err) => console.log(err));
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RegisterForm />
+      <LoginForm />
+      <button type="button" onClick={handleRefreshToken}>
+        Refresh Token
+      </button>
+      <button type="button" onClick={handleCheckUser}>
+        Check User
+      </button>
+      <button type="button" onClick={handleLogout}>
+        Logout
+      </button>
     </>
-  )
+  );
 }
-
-export default App
