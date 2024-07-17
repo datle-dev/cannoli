5756d32e576ed2106aeea30219aece4d5e350202
datle-dev <dle.dev@outlook.com>
2024-07-17 06:59:02 -0400

    Add avatar field to profile model

--- backend/cannoli_profiles/models.py
+++ backend/cannoli_profiles/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class Profile(models.Model):
     user_id = models.OneToOneField('cannoli_auth.User', on_delete=models.CASCADE)
-    about = models.TextField(max_length=256)
+    about = models.TextField(max_length=256, default="")
+    avatar = models.CharField(max_length=9, default="( > u < )")
     update_date = models.DateTimeField()
