6d1deb518e085f7c7a50fc05fb39a0ff83ba4a6b
datle-dev <dle.dev@outlook.com>
2024-07-17 19:48:35 -0400

    Consolidate like functionality into post model

--- backend/cannoli_posts/models.py
+++ backend/cannoli_posts/models.py
@@ -1,11 +1,8 @@
 from django.db import models
+from cannoli_auth.models import User
 
 class Post(models.Model):
-    user_id = models.ForeignKey('cannoli_auth.User', blank=True, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
     content = models.TextField(max_length=256, default="")
     create_date = models.DateTimeField(blank=True)
-
-class Like(models.Model):
-    user_id = models.ForeignKey('cannoli_auth.User', blank=True, on_delete=models.CASCADE)
-    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
-    create_date = models.DateTimeField(blank=True)
+    liked_by = models.ManyToManyField(User, related_name="likes")
