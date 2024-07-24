13c9fabd9232f3ccefa76944b03d81f0dc1acf3e
datle-dev <dle.dev@outlook.com>
2024-07-24 13:49:45 -0400

    Add GET endpoint with user param filter for likes

--- backend/cannoli_posts/views.py
+++ backend/cannoli_posts/views.py
@@ -27,6 +27,17 @@ class PostList(generics.ListCreateAPIView):
 class LikePost(views.APIView):
     permission_classes = [permissions.IsAuthenticated]
 
+    def get(self, request, format=None):
+        qs = Post.objects.all()
+        user_id = self.request.query_params.get('user_id')
+        if user_id is not None:
+            qs = qs.filter(liked_by=user_id)
+            qs = qs.annotate(liked_by_user=Exists(Post.liked_by.through.objects.filter(
+                user_id=self.request.user.pk,
+            )))
+        serializer = PostSerializer(qs, many=True)
+        return Response(serializer.data)
+
     def post(self, request, format=None):
         serializer = LikeSerializer(data=request.data)
         if serializer.is_valid():
