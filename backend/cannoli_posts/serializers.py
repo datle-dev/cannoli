a8032c81bda06bab4cb09da18895be35bc991aac
datle-dev <dle.dev@outlook.com>
2024-07-25 12:11:09 -0400

    Include username in serialized post

--- backend/cannoli_posts/serializers.py
+++ backend/cannoli_posts/serializers.py
@@ -4,6 +4,7 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     like_count = serializers.SerializerMethodField(source='count_likes')
     liked_by_user = serializers.BooleanField(required=False)
+    username = serializers.CharField(source='user_id.username', read_only=True)
 
     def count_likes(self, obj):
         return obj.liked_by.count()
@@ -13,7 +14,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ['id', 'user_id', 'content', 'like_count', 'liked_by_user', 'create_date']
+        fields = ['id', 'user_id', 'username', 'content', 'like_count', 'liked_by_user', 'create_date']
 
 class LikeSerializer(serializers.Serializer):
     post_id = serializers.IntegerField()
