3f1c5dd50524f7f66c69c1390b564648c942f71c
datle-dev <dle.dev@outlook.com>
2024-07-26 08:10:52 -0400

    Add profile_id field to user serializer

--- backend/cannoli_auth/serializers.py
+++ backend/cannoli_auth/serializers.py
@@ -1,17 +1,24 @@
 from rest_framework import serializers
 from .models import User, UserFollowing
+from cannoli_profiles.models import Profile
 
 class UserSerializer(serializers.ModelSerializer):
     follower_count = serializers.SerializerMethodField(read_only=True)
     following_user = serializers.BooleanField(read_only=True)
     followed_by_user = serializers.BooleanField(read_only=True)
+    profile_id = serializers.SerializerMethodField(read_only=True)
 
     def get_follower_count(self, obj):
         return UserFollowing.objects.filter(following_user_id=obj).count()
 
+    def get_profile_id(self, obj):
+        user_id = obj.id
+        profile_id = Profile.objects.get(id=user_id).id
+        return profile_id
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'date_joined', 'follower_count', 'followed_by_user', 'following_user']
+        fields = ['id', 'username', 'profile_id', 'date_joined', 'follower_count', 'followed_by_user', 'following_user']
 
 class UserFollowSerializer(serializers.ModelSerializer):
     class Meta:
