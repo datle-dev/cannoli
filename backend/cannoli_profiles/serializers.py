c84c5ffc70caeb68c7bd40345beb455570fdcf3c
datle-dev <dle.dev@outlook.com>
2024-07-26 08:12:18 -0400

    Add username field to profile serializer

--- backend/cannoli_profiles/serializers.py
+++ backend/cannoli_profiles/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
+from cannoli_auth.models import User
 from cannoli_profiles.models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField(read_only=True)
+
+    def get_username(self, obj):
+        return User.objects.get(pk=obj.user_id.pk).username
+
     class Meta:
         model = Profile
-        fields = ['id', 'user_id', 'about', 'avatar', 'update_date']
+        fields = ['id', 'user_id', 'username', 'about', 'avatar', 'update_date']
