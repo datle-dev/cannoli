274435aca53f98d3bd371f79aea6d120fd881ff5
datle-dev <dle.dev@outlook.com>
2024-07-29 11:02:04 -0400

    Update create post component with Radix UI form

--- frontend/src/components/CreatePost.tsx
+++ frontend/src/components/CreatePost.tsx
@@ -1,18 +1,17 @@
-import { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
+import * as Form from "@radix-ui/react-form";
 
 export default function CreatePost() {
-
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const mutation = useMutation({
+  const postMutation = useMutation({
     mutationFn: async (data) => {
-      console.log('create post mutationFn fired')
+      console.log("create post mutationFn fired");
       return await fetch("http://127.0.0.1:8000/posts/", {
         method: "POST",
         body: JSON.stringify(data),
@@ -28,31 +27,39 @@ export default function CreatePost() {
         .catch((err) => console.log(err));
     },
     onSuccess: () => {
-      console.log('create post mutation success')
-      // queryClient.invalidateQueries({queryKey: ['user']})
-      // navigate("/home")
-    }
-  })
+      console.log("create post mutation success");
+    },
+  });
 
-  function handlePostPost() {
-    console.log('handle post post')
-    mutation.mutate()
+  function handleCreatePost(data) {
+    console.log("handle post post");
+    postMutation.mutate(data);
   }
 
   return (
-    <article>
-      <form onSubmit={handleSubmit(handlePostPost)}>
-        <label htmlFor="content">Post</label>
-        <textarea
-          id="content"
-          placeholder="Write your post"
-          {...register("content", { required: true })}
-        />
-
-        {errors.exampleRequired && <span>This field is required</span>}
-
-        <input type="submit" value="Post" />
-      </form>
-    </article>
+    <div>
+      <Form.Root onSubmit={handleSubmit(handleCreatePost)}>
+        <Form.Field name="content">
+          <Form.Label>Post</Form.Label>
+          <Form.Control asChild>
+            <textarea
+              id="content"
+              placeholder="Write your reply"
+              {...register("content", {
+                required: true,
+                maxLength: {
+                  value: 256,
+                  message: "Max of 256 characters",
+                },
+              })}
+            />
+          </Form.Control>
+          <Form.Message asChild>
+            <p>{errors.content?.message}</p>
+          </Form.Message>
+        </Form.Field>
+        <Form.Submit>Post</Form.Submit>
+      </Form.Root>
+    </div>
   );
 }
