6eeba0f060c21168f1ea442e26f698ebef3c7ea7
datle-dev <dle.dev@outlook.com>
2024-08-02 09:50:22 -0400

    Add following count to user serializer

--- backend/cannoli_auth/serializers.py
+++ backend/cannoli_auth/serializers.py
@@ -4,12 +4,16 @@ from cannoli_profiles.models import Profile
 
 class UserSerializer(serializers.ModelSerializer):
     follower_count = serializers.SerializerMethodField(read_only=True)
+    following_count = serializers.SerializerMethodField(read_only=True)
     following_user = serializers.BooleanField(read_only=True)
     followed_by_user = serializers.BooleanField(read_only=True)
     profile_id = serializers.SerializerMethodField(read_only=True)
 
     def get_follower_count(self, obj):
         return UserFollowing.objects.filter(following_user_id=obj).count()
+
+    def get_following_count(self, obj):
+        return UserFollowing.objects.filter(user_id=obj).count()
 
     def get_profile_id(self, obj):
         user_id = obj.id
@@ -18,7 +22,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'profile_id', 'date_joined', 'follower_count', 'followed_by_user', 'following_user']
+        fields = ['id', 'username', 'profile_id', 'date_joined', 'follower_count', 'following_count', 'followed_by_user', 'following_user']
 
 class UserFollowSerializer(serializers.ModelSerializer):
     class Meta:
